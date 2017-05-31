c4fffc972ae86331e9fe19c2d4113896ee552edf
jonathan-diaz <jonathan.diaz@speeracademy.org>
2017-05-31 13:48:39 -0500

    v0.2.0.0 places pictures

--- content.js
+++ content.js
@@ -10,3 +10,7 @@ chrome.runtime.onMessage.addListener(
     }
   }
 );
+var images = document.getElementsByTagName('img');
+for (var i = 0, l = images.length; i < l; i++) {
+  images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
+};
